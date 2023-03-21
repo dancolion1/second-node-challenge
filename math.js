b2f5997db35e335bba62e28055bc8e9e9c8c0c40
oleabhiele daniel donald <danieloleabhiele@gmail.com>
2023-03-21 11:31:14 +0100

    node code committed

---
+++ math.js
@@ -0,0 +1,11 @@
+
+const sum = (manUtd, Arsenal) => manUtd + Arsenal;
+const sub = (manUtd, Arsenal) => manUtd - Arsenal;
+
+const calculator = {
+  multiply: (p, q) => {
+    return p * q;
+  },
+};
+
+module.exports = { sum, calculator, sub };
